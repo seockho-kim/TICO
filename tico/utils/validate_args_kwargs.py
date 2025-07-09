a3ed23ec86bd2024bee9a4a095882e05d4f1f523
seockho-kim <seockho.kim@samsung.com>
2025-07-09 16:22:50 +0900

    Support round op (#186)
    * Support round op
    
    This commit support round op.
    TICO-DCO-1.0-Signed-off-by: Seockho Kim seockho.kim@samsung.com

--- tico/utils/validate_args_kwargs.py
+++ tico/utils/validate_args_kwargs.py
@@ -935,6 +935,16 @@ class ResizeNearestNeighborArgs:
 
 @enforce_type
 @dataclass
+class RoundArgs:
+    """
+    round(Tensor self) -> Tensor
+    """
+
+    input: torch.fx.Node
+
+
+@enforce_type
+@dataclass
 class RsqrtArgs:
     """
     rsqrt(Tensor self) -> Tensor
