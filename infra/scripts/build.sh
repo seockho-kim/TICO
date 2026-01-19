aaf55d782861eb8c9232d11329c4105a08db24fb
Dayoung Lee <dayoung.lee@samsung.com>
2026-01-19 15:08:35 +0900

    Use pyproject.toml (#432)
    * Use pyproject.toml
    
    Let's use pyproject.toml.
    This change also includes..
    - bump version to 0.2.0 and tag nightly as 0.2.0.devYYMMDD
    - use versioiningit pypi tool to generate dev tag
    
    TICO-DCO-1.0-Signed-off-by: Dayoung Lee <dayoung.lee@samsung.com>

--- infra/scripts/build.sh
+++ infra/scripts/build.sh
@@ -1,1 +1,9 @@
-python3 setup.py bdist_wheel --dist-dir dist
+#!/bin/bash
+
+# Install Required Package
+#
+# NOTE To add additional build dependencies, append to `requires` field of [build-system] in pyproject.toml 
+python3 -m pip install build
+
+# Build
+python3 -m build
